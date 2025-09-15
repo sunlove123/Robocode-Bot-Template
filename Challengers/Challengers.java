f5207e07268b61ec1d0ad6ee7e27484cd7e44d9f
ashudill <107581048+ashudill@users.noreply.github.com>
2025-09-15 13:54:29 +0100

    Update Challengers.java

--- Challengers/Challengers.java
+++ Challengers/Challengers.java
@@ -1,65 +1,76 @@
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 import dev.robocode.tankroyale.botapi.graphics.Color;
-//import dev.robocode.tankroyale.botapi.util.Ut
-
+import dev.robocode.*;
+import dev.robocode.tankroyale.botapi.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.*;
 
 public class Challengers extends Bot {
 
-    // The main method starts our bot
-    public static void main(String[] args)
-    {
+    private int moveDirection = 1;
+
+    public static void main(String[] args) {
         new Challengers().start();
     }
 
-    // Constructor, which loads the bot config file
     Challengers() {
         super(BotInfo.fromFile("Challengers.json"));
     }
 
-    // Called when a new round is started -> initialize and do some movement
     @Override
     public void run() {
+        // Bot color setup
         setBodyColor(Color.YELLOW_GREEN);
         setGunColor(Color.RED);
         setRadarColor(Color.YELLOW_GREEN);
 
-        setAdjustRadarForBodyTurn(true);
-        setAdjustRadarForGunTurn(true);
-
-        // spin Radar
-       // while (true) {
-         //   setTurnRadarRight(360);
-        //}
-
-        // Repeat while the bot is running
         while (isRunning()) {
-            forward(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
-            setTurnRadarRight(360);
+            // Basic patrol movement
+            forward(100 * moveDirection);
+            turnGunRight(360); // Scan while moving
+            turnRadarRight(360); // Spin radar to scan
+
+            // Check if close to wall
+            if (getX() < 100 || getX() > getArenaWidth() - 100 ||
+                    getY() < 100 || getY() > getArenaHeight() - 100) {
+                moveDirection *= -1; // Reverse direction
+                back(100); // Move away from wall
+            }
         }
     }
 
-    // We saw another bot -> fire!
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-       // double angle = Utils.normalRelativeAngleDegrees(e.getBearing() + 90);
-      //  if (e.getD)
-        fire(0);
+        // Aim gun at enemy
+        Point2D.Double enemyPos = new Point2D.Double(e.getX(),e.getY());
+       // double angleToTarget = calcBearing(enemyPos);
+      //  turnGunRight(normalRelativeAngleDegrees(angleToTarget - getGunDirection()));
 
+        // Fire with power depending on distance
+        double distance = distanceTo(e.getX(), e.getY());
+        double firepower = distance < 150 ? 3.0 : 1.5;
 
+        if (getEnergy() > 10) {
+            fire(firepower);
+        }
     }
 
-    // We were hit by a bullet -> turn perpendicular to the bullet
     @Override
     public void onHitByBullet(HitByBulletEvent e) {
-        // Calculate the bearing to the direction of the bullet
-        var bearing = calcBearing(e.getBullet().getDirection());
+        // Dodge: turn perpendicular to bullet
+        double bulletDir = e.getBullet().getDirection();
+        turnLeft(normalRelativeAngleDegrees(bulletDir - getDirection()) + 90);
+        forward(50); // Move away
+    }
 
-        // Turn 90 degrees to the bullet direction based on the bearing
-        turnLeft(90 - bearing);
+    // Helper to calculate relative angle
+    private double normalRelativeAngleDegrees(double angle) {
+        angle = angle % 360;
+        if (angle > 180) angle -= 360;
+        if (angle < -180) angle += 360;
+        return angle;
     }
 }
-
